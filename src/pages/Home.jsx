296203aa244ef4d3f8ebea739476b7b1e9070ff2
Konstantin <konvod93@gmail.com>
2022-12-04 21:03:39 +0200

    save status in state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
@@ -11,10 +11,16 @@ import { CommentsBlock } from '../components/CommentsBlock';
 import { fetchPosts } from '../redux/slices/post';
 
 export const Home = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); 
+
+  const { posts, tags } = useSelector(state => state.posts);
+
+  const isPostsLoading = posts.status === 'loading';
+
   React.useEffect(() => {
     dispatch(fetchPosts())
   }, []);
+  
   return (
     <>
       <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
@@ -23,21 +29,16 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostsLoading ? <Post key={index} isLoading={true}/> : (
             <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
+              id={obj._id}
+              title={obj.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'12 июня 2022 г.'}
-              viewsCount={150}
+              user={obj.user}
+              createdAt={obj.createdAt}
+              viewsCount={obj.viewsCount}
               commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
-              isLoading={true}
+              tags={obj.tags}              
               isEditable
             />
           ))}
