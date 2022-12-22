d277911c2d6d0826929542b1116ebbcc6e61f2a1
Konstantin <konvod93@gmail.com>
2022-12-22 20:45:54 +0200

    form of post creation

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
+import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 import styles from './AddPost.module.scss';
 
+
 export const AddPost = () => {
+  const isAuth = useSelector(selectIsAuth);
   const imageUrl = '';
   const [value, setValue] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState(''); 
+
+
 
   const handleChangeFile = () => {};
 
@@ -34,6 +44,10 @@ export const AddPost = () => {
     [],
   );
 
+  if (window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to="/" />
+  }
+
   return (
     <Paper style={{ padding: 30 }}>
       <Button variant="outlined" size="large">
@@ -54,9 +68,18 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={title}
+        onChange={e => setTitle(e.target.value)}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField 
+      value={tags}
+      onChange={e => setTags(e.target.value)}
+      classes={{ root: styles.tags }} 
+      variant="standard" 
+      placeholder="Тэги" 
+      fullWidth 
+      />
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
